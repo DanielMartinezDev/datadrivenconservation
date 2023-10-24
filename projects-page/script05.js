04ee774193b7495e9ff384e58c853506e876512f
Bryan Schmidt <bryan.schmidt03@icloud.com>
2023-10-24 14:05:30 +0200

    Clean up + favicon

--- projects-page/script05.js
+++ projects-page/script05.js
@@ -1,12 +1,12 @@
 function toggle() {
-    let blur = document.getElementById('blur');
-    blur.classList.toggle('active');
-    let popup = document.getElementById('popup');
-    popup.classList.toggle('active');
-    let popup02 = document.getElementById('popup02');
-    popup02.classList.toggle(null);
-    let popup03 = document.getElementById('popup03');
-    popup03.classList.toggle(null);
+  let blur = document.getElementById('blur');
+  blur.classList.toggle('active');
+  let popup = document.getElementById('popup');
+  popup.classList.toggle('active');
+  let popup02 = document.getElementById('popup02');
+  popup02.classList.toggle(null);
+  let popup03 = document.getElementById('popup03');
+  popup03.classList.toggle(null);
 }
 
 function toggle02() {
